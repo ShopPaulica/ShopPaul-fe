db22978699e92d277738e53ae3a5ff293cbf5e37
ionitaandrei66 <ionita_andrei_2002@yahoo.com>
2026-01-11 18:30:47 +0200

    .

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,20 +1,29 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsServices} from '../../services/products.services';
 import {ProductModel} from '../../shared/interfaces/product.model';
+import {FormsModule} from '@angular/forms';
+import {of} from 'rxjs';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    FormsModule,
+    DecimalPipe
+  ],
   templateUrl: './products.component.html',
   standalone: true,
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
   protected products: ProductModel[] = [];
+  public search: string = '';
 
   constructor(private _products: ProductsServices) {}
 
   ngOnInit(): void {
   this.products = this._products.getProducts()
   }
+
+  protected readonly of = of;
 }
--- src/app/services/products.services.ts
+++ src/app/services/products.services.ts
@@ -14,50 +14,59 @@ export class ProductsServices {
         title: 'Test',
         description: 'test DEscription',
         price: 100,
+        image: 'https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 1,
         title: 'Test1',
         description: 'test1 DEscription',
         price: 10,
+        image: 'https://images.pexels.com/photos/307008/pexels-photo-307008.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 2,
         title: 'Test4',
         description: 'test5 DEscription',
         price: 1012.124,
+        image: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 3,
         title: 'Test3',
         description: 'test3 DEscription',
         price: 12,
+        image: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 19,
         title: 'Testwrwq4',
         description: 'tesqwrwqt5 DEscription',
         price: 1.21,
+        image: 'https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 22,
         title: 'Test4',
         description: 'test5 DEscription',
         price: 1012.124,
+        image: 'https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 33,
         title: 'Test3',
         description: 'test3 DEscription',
         price: 12,
+        image: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       },
       {
         id: 194,
         title: 'Testwrwq4',
         description: 'tesqwrwqt5 DEscription',
         price: 1.21,
+        image: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
       }
-    ]
+    ];
   }
+
 
 }
