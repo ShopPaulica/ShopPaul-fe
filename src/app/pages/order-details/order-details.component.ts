6006596a44fcbaf1413b89975c075d8f4717eb77
ionitaandrei66 <ionita_andrei_2002@yahoo.com>
2026-01-30 16:38:01 +0200

    deploy action

--- src/app/pages/order-details/order-details.component.ts
+++ src/app/pages/order-details/order-details.component.ts
@@ -3,10 +3,15 @@ import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {ShoppingCartProduct} from '../../shared/interfaces/shopping-cart-product.model';
 import {CartService} from '../../services/cart.service';
 import {NavigationService} from '../../services/router-service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-order-details',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    DecimalPipe
+  ],
   templateUrl: './order-details.component.html',
   standalone: true,
   styleUrl: './order-details.component.scss'
@@ -14,12 +19,37 @@ import {NavigationService} from '../../services/router-service';
 export class OrderDetailsComponent implements OnInit{
   public shoppingCart: ShoppingCartProduct[] = []
   private readonly destroyRef = inject(DestroyRef);
+  public formDetails!: FormGroup;
 
-  constructor(private cartService: CartService) {}
+  public get getTotal(): number {
+    return this.cartService.getTotal;
+  }
+
+  constructor(private cartService: CartService, private _fb: FormBuilder) {}
 
   ngOnInit() {
     this.cartService.cart$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((products: ShoppingCartProduct[]) => {
       this.shoppingCart = products;
     })
+
+    this.formDetails = this._fb.group({
+      name: [null as File | null, [Validators.required]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      vin: ['', [Validators.required]],
+      details: ['', [Validators.required]],
+    })
+  }
+
+  public removeOne(item: ShoppingCartProduct): void {
+    this.cartService.removeOne(item)
+  }
+
+  public addOne(item: ShoppingCartProduct): void {
+    this.cartService.addOne(item)
+  }
+
+  public isInCart(id: number | undefined): number {
+    return id ? this.cartService.howManyAreOfOne(id) ?? 0 : 0;
   }
 }
