003d264979a68afdf05f45d80c7c781e6489ab6d
ionitaandrei66 <ionita_andrei_2002@yahoo.com>
2026-02-01 22:01:36 +0200

    env

--- src/app/shared/services/products.services.ts
+++ src/app/shared/services/products.services.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {CreateProductModel, ProductModel} from '../interfaces/product.model';
+import {CreateProductModel} from '../interfaces/product.model';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {from, Observable} from 'rxjs';
