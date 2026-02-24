4adf936d396aee0abfe6ece29e4495683c6eb7bd
ionitaandrei66 <ionita_andrei_2002@yahoo.com>
2026-02-25 01:01:47 +0200

    env

--- src/app/shared/services/vehicles.services.ts
+++ src/app/shared/services/vehicles.services.ts
@@ -6,13 +6,30 @@ import {BehaviorSubject, catchError, EMPTY, finalize, from, Observable, tap} fro
 import {NotificationService} from './notification.service';
 import {VehiclesModel} from '../interfaces/vehicles.model';
 
+//todo token service and another interface service
 @Injectable({
   providedIn: 'root'
 })
 export class VehiclesServices {
-  private readonly _productsPageSubject = new BehaviorSubject<ProductsPaginationModel | null>(null);
-  public readonly productsPage$: Observable<ProductsPaginationModel | null> =
-    this._productsPageSubject.asObservable();
+  private readonly _brandFiltersSubject = new BehaviorSubject<string[] | null>(null);
+  public readonly brandFiltersPage$: Observable<string[] | null> =
+    this._brandFiltersSubject.asObservable();
+
+  private readonly _modelFiltersSubject = new BehaviorSubject<string[] | null>(null);
+  public readonly modelFiltersPage$: Observable<string[] | null> =
+    this._modelFiltersSubject.asObservable();
+
+  private readonly _fuelFiltersSubject = new BehaviorSubject<string[] | null>(null);
+  public readonly fuelFiltersPage$: Observable<string[] | null> =
+    this._fuelFiltersSubject.asObservable();
+
+  private readonly _engineFiltersSubject = new BehaviorSubject<string[] | null>(null);
+  public readonly engineFiltersPage$: Observable<string[] | null> =
+    this._engineFiltersSubject.asObservable();
+
+  private readonly _powerFiltersSubject = new BehaviorSubject<string[] | null>(null);
+  public readonly powerFiltersPage$: Observable<string[] | null> =
+    this._powerFiltersSubject.asObservable();
 
   private readonly _loadingSubject = new BehaviorSubject<boolean>(false);
   public readonly loading$ = this._loadingSubject.asObservable();
@@ -23,14 +40,56 @@ export class VehiclesServices {
   constructor(
     private readonly _http: HttpClient,
     private readonly _notificationService: NotificationService
-  ) {}
+  ) {
+    this.getFilters('BMW','X6')
+  }
 
-  public loadProducts(page: number): void {
-    this._loadingSubject.next(true);
-    this._errorSubject.next(null);
+  public getFilters(brand?: string, model?: string, fuel?: string, engine?: string, power?: string): void {
+    let vehicle: string;
+    let params: Record<string,string> = {};
+    if(brand) {
+      vehicle = 'model';
+      params['brand'] = brand;
+      if(model) {
+        vehicle = 'fuel';
+        params['model'] = model;
+        if(fuel) {
+          params['fuel'] = fuel;
+          vehicle = 'engine';
+          if(engine) {
+            params['engine'] = engine;
+            vehicle = 'power';
+          }
+        }
+      }
+    } else {
+      vehicle = 'brand'
+    }
 
-    this.getProducts(page).pipe(
-      tap((res: ProductsPaginationModel) => this._productsPageSubject.next(res)),
+    this.fetchFilter(vehicle, params).pipe(
+      tap((res: string[]) => {
+        if(vehicle === 'brand') {
+          this._brandFiltersSubject.next(res);
+          this._modelFiltersSubject.next(null);
+          this._fuelFiltersSubject.next(null);
+          this._engineFiltersSubject.next(null);
+          this._powerFiltersSubject.next(null);
+        } else if(vehicle === 'model') {
+          this._modelFiltersSubject.next(res);
+          this._fuelFiltersSubject.next(null);
+          this._engineFiltersSubject.next(null);
+          this._powerFiltersSubject.next(null);
+        } else if(vehicle === 'fuel') {
+          this._fuelFiltersSubject.next(res);
+          this._engineFiltersSubject.next(null);
+          this._powerFiltersSubject.next(null);
+        } else if(vehicle === 'engine') {
+          this._engineFiltersSubject.next(res);
+          this._powerFiltersSubject.next(null);
+        } else if(vehicle === 'power') {
+          this._powerFiltersSubject.next(res);
+        }
+      }),
       catchError((err) => {
         this._errorSubject.next(err?.error?.message ?? 'Eroare la încărcarea produselor');
         return EMPTY;
@@ -65,8 +124,10 @@ export class VehiclesServices {
     return this._http.post(`${environment.apiUrl}/vehicles`, fd);
   }
 
-  public getProducts(page: number): Observable<ProductsPaginationModel> {
-    return this._http.post<ProductsPaginationModel>(`${environment.apiUrl}/products/list`,{ page: page });
+  private fetchFilter(field: string, params: Record<string,string> = {}) {
+    return this._http.get<string[]>(`${environment.apiUrl}/vehicles/filters`, {
+      params: { field, ...params }
+    });
   }
 
   public deleteProduct(id: string): Observable<Object> {
